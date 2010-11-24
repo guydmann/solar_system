861bf1de41745f8d449fa8a5837685a2affe79cb
Guy Mann <guydmann@gmail.com>
2010-11-23 19:10:55 -0500

    added a bunch of examples

---
+++ solar_system_FS.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<body style="background-color:black">
+
+<script type="text/javascript" src="./Three.js"></script>
+<script type="text/javascript" src="./base.js"></script>
+
+<?php $_GET['width']='window.innerWidth'; $_GET['height']='window.innerHeight'; include("./solar_system_js.php"); ?>
+</body>
+</html>
+
